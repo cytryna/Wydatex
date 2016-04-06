5ab2f138fc929943e4049777b537577ce6020bef
rwichrowski <radoslaw.wichrowski@efigence.com>
2016-04-06 08:31:48 +0200

    init

---
+++ app/src/main/java/com/example/radek/wydatex/InputActivity.java
@@ -0,0 +1,43 @@
+package com.example.radek.wydatex;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InputActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        Button ok = (Button) findViewById(R.id.ok);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                TextView nameTextView = (TextView) findViewById(R.id.name);
+                RadioButton genderMale = (RadioButton) findViewById(R.id.male);
+                RadioButton genderFemale = (RadioButton) findViewById(R.id.female);
+
+                String name = nameTextView.getText().toString();
+                String gender = "";
+
+                if(genderMale.isChecked())
+                    gender = "Facet! ";
+
+                if(genderFemale.isChecked())
+                    gender = "Kobieta! ";
+
+
+                Toast.makeText(getApplicationContext(), "O " + gender + name, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/radek/wydatex/MainActivity.java
+++ app/src/main/java/com/example/radek/wydatex/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.radek.wydatex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        System.out.println("Wciśniecie przycisku");
     }
 }
